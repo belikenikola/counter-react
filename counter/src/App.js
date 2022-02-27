da05202a31abc5baf724b4382a8402669811af1a
Nikola <belikenikola@gmail.com>
2022-02-27 02:21:46 +0100

    Update

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import './App.css';
+import plus from './assets/plus.svg';
+import minus from './assets/minus.svg';
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [fetchData, setFetchData] = useState({});
+  const [loading , setLoading] = useState(false);
 
   const arr = [
     "eslint/eslint",
-    "oakwood/front-end-questions",
     "babel/babel",
     "webpack/webpack",
     "storybooks/storybook",
@@ -16,20 +19,34 @@ function App() {
     "expressjs/express",
   ];
 
-  useEffect(() => {
-    Axios.get(`https://api.github.com/repos/${arr[counter]}`).then((r) => {
-      setFetchData(r.data);
-      console.log(r.data);
-    });
-  }, [arr, counter]);
+  useEffect( async() => {
+
+    setLoading(true);
+    const r = await Axios.get(`https://api.github.com/repos/${arr[counter]}`);
+    setFetchData(r.data);
+    console.log(r.data);
+    setLoading(false);
+    
+
+
+    // Axios.get(`https://api.github.com/repos/${arr[counter]}`).then((r) => {
+    //   setFetchData(r.data);
+    //   console.log(r.data);
+    // });
+  }, [ counter]);
 
   const increment = () => {
-    if (counter === arr.length - 1) {
-      setCounter("End");
-    } else {
+    // if (counter === arr.length - 1) {
+    //   setCounter(0);
+    // } else {
+    //   setCounter((prevState) => prevState + 1);
+    // }
+
+    if (counter < arr.length - 1) {
       setCounter((prevState) => prevState + 1);
     }
   };
+  
   const decrement = () => {
     if (counter === 0) {
       setCounter(0);
@@ -40,16 +57,24 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={decrement}>decrement</button>
-      <p>{counter}</p>
-      <button onClick={increment}>increment</button>
+      <div className="wrapper">
+        <button className="decrement" onClick={decrement}>
+          - decrement
+        </button>
+        <p> Counter : {counter}</p>
+        <button className="increment" disable onClick={increment}>
+          + increment
+        </button>
+      </div>
+     
+     {loading ? <div> Loading... </div> :
       <p>
         {" "}
         <span>Full Name: {fetchData.full_name}</span>
         <br /> <span>Description: {fetchData.description}</span>
         <br /> <span>Amount of stars: {fetchData.stargazers_count}</span>
         <br />
-      </p>
+      </p>}
     </div>
   );
 }
