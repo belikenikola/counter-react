af419a5a607e461227b6994842b4ce403862fbe4
Nikola <belikenikola@gmail.com>
2022-02-27 19:55:29 +0100

    Cleaning the code

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import Axios from "axios";
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 import plus from './assets/plus.svg';
 import minus from './assets/minus.svg';
@@ -7,46 +7,31 @@ import minus from './assets/minus.svg';
 function App() {
   const [counter, setCounter] = useState(0);
   const [fetchData, setFetchData] = useState({});
-  const [loading , setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const arr = [
-    "eslint/eslint",
-    "babel/babel",
-    "webpack/webpack",
-    "storybooks/storybook",
-    "facebook/react",
-    "reactjs/redux",
-    "expressjs/express",
+    'eslint/eslint',
+    'babel/babel',
+    'webpack/webpack',
+    'storybooks/storybook',
+    'facebook/react',
+    'reactjs/redux',
+    'expressjs/express',
   ];
 
-  useEffect( async() => {
-
+  useEffect(async () => {
     setLoading(true);
     const r = await Axios.get(`https://api.github.com/repos/${arr[counter]}`);
     setFetchData(r.data);
-    console.log(r.data);
     setLoading(false);
-    
-
-
-    // Axios.get(`https://api.github.com/repos/${arr[counter]}`).then((r) => {
-    //   setFetchData(r.data);
-    //   console.log(r.data);
-    // });
-  }, [ counter]);
+  }, [counter]);
 
   const increment = () => {
-    // if (counter === arr.length - 1) {
-    //   setCounter(0);
-    // } else {
-    //   setCounter((prevState) => prevState + 1);
-    // }
-
     if (counter < arr.length - 1) {
       setCounter((prevState) => prevState + 1);
     }
   };
-  
+
   const decrement = () => {
     if (counter === 0) {
       setCounter(0);
@@ -66,15 +51,18 @@ function App() {
           + increment
         </button>
       </div>
-     
-     {loading ? <div> Loading... </div> :
-      <p>
-        {" "}
-        <span>Full Name: {fetchData.full_name}</span>
-        <br /> <span>Description: {fetchData.description}</span>
-        <br /> <span>Amount of stars: {fetchData.stargazers_count}</span>
-        <br />
-      </p>}
+
+      {loading ? (
+        <div> Loading... </div>
+      ) : (
+        <p>
+          {' '}
+          <span>Full Name: {fetchData.full_name}</span>
+          <br /> <span>Description: {fetchData.description}</span>
+          <br /> <span>Amount of stars: {fetchData.stargazers_count}</span>
+          <br />
+        </p>
+      )}
     </div>
   );
 }
